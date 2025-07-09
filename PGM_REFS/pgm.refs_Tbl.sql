7a5e51067d52c457b3c79cc62e5c21debf90a0e0
Harshith <133903106+Harshitho@users.noreply.github.com>
2025-07-09 15:10:09 +0530

    Removed LENNONS, slennon, SUB and A8%%TS

--- PGM_REFS/pgm.refs_Tbl.sql
+++ PGM_REFS/pgm.refs_Tbl.sql
@@ -1,8 +1,8 @@
 -- Table created by the PGM_REFS SQL procedure
--- Change LENNONSB to your library name to suit and make
+-- Change USERB to your library name to suit and make
 -- a similar change in PGM_REFS.
-DROP TABLE LENNONSB.REFS;
-create or replace  TABLE LENNONSB.REFS (
+DROP TABLE USERB.REFS;
+create or replace  TABLE USERB.REFS (
      DEPTH          integer
     ,CALLER_LIBRARY VarCHAR(10) NOT NULL
     ,CALLER_NAME    VarCHAR(10) NOT NULL
@@ -11,7 +11,7 @@ create or replace  TABLE LENNONSB.REFS (
     ,USES_LIBRARY   VarCHAR(10) NOT NULL
     ,USES_NAME      VarCHAR(10) NOT NULL
     ,USES_TYPE      VarCHAR(10) NOT NULL
-    ,CONSTRAINT     LENNONSB.refs_pk PRIMARY KEY (
+    ,CONSTRAINT     USERB.refs_pk PRIMARY KEY (
         CALLER_LIBRARY
         ,CALLER_NAME
         ,CALLER_TYPE
